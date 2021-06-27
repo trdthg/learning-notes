293e64b433216959e075a4d78e03ad9ea9bc9268
trdthg <trdthg@outlook.com>
2021-06-27 22:56:33 +0800

    commit

--- js/express-starter/src/routes/yym.js
+++ js/express-starter/src/routes/yym.js
@@ -3,12 +3,32 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render("yym/index", { title: "Tencent CloudBase + Express" })
 });
 
-router.get("/automap", function (req, res, next) {
-    res.render("yym_automap", { title: "Tencent CloudBase + Express" })
+router.get('/Cloud-bfs-canvas', function(req, res, next) {
+  res.render("yym/Cloud-bfs-canvas", { title: "Tencent CloudBase + Express" })
 });
 
+router.get('/Cloud-bfs', function(req, res, next) {
+  res.render("yym/Cloud-bfs", { title: "Tencent CloudBase + Express" })
+});
+
+
+router.get('/Cloud-dfs-Animation-A-canvas', function(req, res, next) {
+  res.render("yym/Cloud-dfs-Animation-A-canvas", { title: "Tencent CloudBase + Express" })
+});
+
+router.get('/Cloud-dfs-Animation-A', function(req, res, next) {
+  res.render("yym/Cloud-dfs-Animation-A", { title: "Tencent CloudBase + Express" })
+});
+
+router.get('/Cloud-dfs-Animation-B-canvas', function(req, res, next) {
+  res.render("yym/Cloud-dfs-Animation-B-canvas", { title: "Tencent CloudBase + Express" })
+});
+
+router.get('/Cloud-dfs-Animation-B', function(req, res, next) {
+  res.render("yym/Cloud-dfs-Animation-B", { title: "Tencent CloudBase + Express" })
+});
 
 module.exports = router;
