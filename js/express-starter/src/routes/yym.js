53abd480f94e6d514c1efc7fdc738b45ad600a14
trdthg <trdthg@outlook.com>
2021-06-27 11:08:15 +0800

    commit

---
+++ js/express-starter/src/routes/yym.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get("/yym_automap", function (req, res, next) {
+    res.render("yym_automap", { title: "Tencent CloudBase + Express" })
+});
+
+
+module.exports = router;
