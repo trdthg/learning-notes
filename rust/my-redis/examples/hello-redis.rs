59655e06684458dd86bdaf5496160b91a180f9dc
trdthg <trdthg@outlook.com>
2021-08-07 17:57:52 +0800

    commit

---
+++ rust/my-redis/examples/hello-redis.rs
@@ -0,0 +1,34 @@
+
+use mini_redis::{ client, Result };
+
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+
+    // async only allowed inside `async` functions and blocks
+    // calling an async fn returns a value representing the operation. This is conceptually analogous to a zero-argument closure. To actually run the operation, you should use the .await operator on the return value.
+    async_test(); 
+
+    // Open a connection to the mini-redis address
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    // Set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+    // Get key "hello"
+    let result = client.get("hello").await?;
+    println!("hello: {:?}", result);
+
+
+
+    Ok(())
+}
+
+async fn async_test() {
+
+    async fn say_hello() {
+        println!("world")
+    }
+
+    println!("hello");
+    say_hello().await;
+
+}
